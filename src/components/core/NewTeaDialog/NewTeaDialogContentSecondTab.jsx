a3d2d4c46eaadf32c2fef9cc1b5f479dc05fbbe4
pouannes <pierreouannes@gmail.com>
2020-12-31 02:22:44 +0100

    add work on add new tea second tab

--- src/components/core/NewTeaDialog/NewTeaDialogContentSecondTab.jsx
+++ src/components/core/NewTeaDialog/NewTeaDialogContentSecondTab.jsx
@@ -7,16 +7,18 @@ import {
   FormControlLabel,
   Checkbox,
   FormControl,
+  Button,
+  TextField,
 } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-  mainContent: {
-    width: "100%",
-  },
   sideBar: {
     width: "100%",
     height: "100%",
     overflow: "scroll",
+    // "&::-webkit-scrollbar": {
+    //   width: "11px",
+    // },
   },
   sidebarTitle: {
     marginBottom: "1.2rem",
@@ -39,6 +41,41 @@ const useStyles = makeStyles((theme) => ({
   },
   primaryCheckbox: {
     marginBottom: "0.5rem",
+  },
+  mainContent: {
+    width: "100%",
+    height: "100%",
+    overflow: "scroll",
+  },
+  addNewComment: {
+    border: "2px dashed grey",
+    width: "100%",
+    height: "60px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    cursor: "pointer",
+  },
+  newComment: {
+    width: "100%",
+    height: "300px",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+  },
+  newCommentStars: {
+    width: "100%",
+    height: "100px",
+  },
+  commentTextField: {
+    width: "100%",
+    flexGrow: "1",
+  },
+  newCommentButtons: {
+    alignSelf: "flex-end",
+  },
+  submitButton: {
+    marginLeft: "20px",
   },
 }));
 
@@ -99,9 +136,103 @@ const checkboxes = [
   },
 ];
 
-function NewTeaDialogContentSecondTab({ checkboxStateTodo }) {
+const CheckboxSidebar = ({ state, handleChange }) => {
   const classes = useStyles();
+  return (
+    <div className={classes.sideBar}>
+      <Typography className={classes.sidebarTitle}>
+        This tea goes well with...
+      </Typography>
+      <div className={classes.checkboxesContainer}>
+        {checkboxes.map((mainCheckbox) => (
+          <Fragment key={mainCheckbox.name}>
+            {mainCheckbox.type === "category" ? (
+              <>
+                <Typography>{mainCheckbox.label}</Typography>
+                <FormControl className={classes.checkboxesSubContainer}>
+                  {mainCheckbox.sub.map((subCheckbox) => (
+                    <FormControlLabel
+                      key={subCheckbox.name}
+                      control={
+                        <Checkbox
+                          checked={state[subCheckbox.name]}
+                          onChange={handleChange}
+                          name={subCheckbox.name}
+                          color="primary"
+                        />
+                      }
+                      label={subCheckbox.label}
+                    />
+                  ))}
+                </FormControl>
+              </>
+            ) : (
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={state[mainCheckbox.name]}
+                    onChange={handleChange}
+                    name={mainCheckbox.name}
+                    color="primary"
+                  />
+                }
+                label={mainCheckbox.label}
+                className={classes.primaryCheckbox}
+              />
+            )}
+          </Fragment>
+        ))}
+      </div>
+    </div>
+  );
+};
 
+CheckboxSidebar.propTypes = {
+  state: PropTypes.object.isRequired,
+  handleChange: PropTypes.func.isRequired,
+};
+
+const MainContent = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.mainContent}>
+      <div className={classes.addNewComment} onClick={() => alert("todo")}>
+        <Typography>Add a new comment</Typography>
+      </div>
+      <div className={classes.newComment}>
+        <div className={classes.newCommentStars}>stars</div>
+        <div className={classes.commentTextField}>
+          <TextField
+            // value={description}
+            // onChange={handleDescriptionChange}
+            margin="none"
+            label="Enter your comment here"
+            type="text"
+            multiline
+            rows={6}
+            fullWidth
+            variant="outlined"
+          />
+        </div>
+        <div className={classes.newCommentButtons}>
+          <Button variant="outlined" color="primary" size="small">
+            Cancel
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            className={classes.submitButton}
+            size="small"
+          >
+            Save comment
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+function NewTeaDialogContentSecondTab({ checkboxStateTodo }) {
   // ok so that's definitly way too complicated than it should be but it's the best
   // I could do with the current data format
   // either change data format or find a better way to do this to refactor the horror below
@@ -128,54 +259,8 @@ function NewTeaDialogContentSecondTab({ checkboxStateTodo }) {
   };
   return (
     <>
-      <div className={classes.mainContent}>
-        <Typography>I'm the second tab main content</Typography>
-      </div>
-      <div className={classes.sideBar}>
-        <Typography className={classes.sidebarTitle}>
-          This tea goes well with...
-        </Typography>
-        <div className={classes.checkboxesContainer}>
-          {checkboxes.map((mainCheckbox) => (
-            <Fragment key={mainCheckbox.name}>
-              {mainCheckbox.type === "category" ? (
-                <>
-                  <Typography>{mainCheckbox.label}</Typography>
-                  <FormControl className={classes.checkboxesSubContainer}>
-                    {mainCheckbox.sub.map((subCheckbox) => (
-                      <FormControlLabel
-                        key={subCheckbox.name}
-                        control={
-                          <Checkbox
-                            checked={state[subCheckbox.name]}
-                            onChange={handleChange}
-                            name={subCheckbox.name}
-                            color="primary"
-                          />
-                        }
-                        label={subCheckbox.label}
-                      />
-                    ))}
-                  </FormControl>
-                </>
-              ) : (
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      checked={state[mainCheckbox.name]}
-                      onChange={handleChange}
-                      name={mainCheckbox.name}
-                      color="primary"
-                    />
-                  }
-                  label={mainCheckbox.label}
-                  className={classes.primaryCheckbox}
-                />
-              )}
-            </Fragment>
-          ))}
-        </div>
-      </div>
+      <MainContent />
+      <CheckboxSidebar state={state} handleChange={handleChange} />
     </>
   );
 }
