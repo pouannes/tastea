6a128c387c71686146b7c3a3e35013abdf3b5fe0
pouannes <pierreouannes@gmail.com>
2021-06-27 04:02:35 +0200

    add beginning of fix but ts is suffering

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,62 @@
+import { useState, useEffect } from 'react';
 import Head from 'next/head';
+import Image from 'next/image';
+import { supabase } from '../utils/supabaseClient';
+import TemperatureIcon from '../public/temperature.svg';
+import TeaIcon from '../public/tea.svg';
+
+const formatTime = (time: number): string => {
+  if (time < 60) return `${time}s`;
+  if (time % 60 === 0) return `${time / 60}min`;
+  if (time % 60 < 30) return `${parseInt(time / 60)}min`;
+  return `${parseInt(time / 60)}min30`;
+};
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+  const [teas, setTeas] = useState(null);
+
+  useEffect(() => {
+    const fetchTeas = async () => {
+      try {
+        let { data, error, status } = await supabase.from('teas').select(`name, 
+                   brand_time_s, 
+                   brand_temperature,
+                   brand:brand_id (name)`);
+
+        console.log(data);
+        setTeas(data);
+      } catch (error) {}
+    };
+
+    fetchTeas();
+  }, []);
+
   return (
     <div className="h-screen bg-bgDefault">
       <Head>
         <title>Tastea</title> ===
       </Head>
-      <p className="text-textPrimary">Hello</p>
+      <div className="flex flex-col items-center justify-center w-5/6 h-full m-auto">
+        <div className="grid w-full grid-cols-4 mb-2">
+          <p className="text-textPrimary">Tea</p>
+          <p className="text-textPrimary">Brand</p>
+          <TemperatureIcon className="w-8 h-8 fill-current text-textPrimary" />
+          <TeaIcon className="w-8 h-8 fill-current text-textPrimary" />
+        </div>
+        {teas ? teas.map((tea) => <TeaLine key={tea.name} {...tea} />) : null}
+      </div>
     </div>
   );
 }
+
+const TeaLine = ({ name, brand_time_s, brand_temperature, brand }) => {
+  return (
+    <div className="grid w-full grid-cols-4 py-2 border-t">
+      <p className="text-textPrimary">{name}</p>
+      <p className="text-textPrimary">{brand.name}</p>
+      <p className="text-textPrimary">{formatTime(brand_time_s)}</p>
+      <p className="text-textPrimary">{brand_temperature}</p>
+    </div>
+  );
+};
