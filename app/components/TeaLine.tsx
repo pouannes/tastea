ff6458bab341abac46015b88a3cdb68435ee3062
pouannes <pierreouannes@gmail.com>
2021-07-05 07:45:40 +0200

    add better icons

--- app/components/TeaLine.tsx
+++ app/components/TeaLine.tsx
@@ -1,12 +1,17 @@
 import { useState } from 'react';
 
 import { PencilAltIcon, TrashIcon } from '@heroicons/react/solid';
+import {
+  GlobeIcon,
+  ClockIcon,
+  SparklesIcon,
+  TagIcon,
+} from '@heroicons/react/outline';
 
 import { tea } from '@/types/api';
 import { formatTime } from '@/utils';
 import { IconButton, ConfirmationDialog } from '@/components/core';
 import TemperatureIcon from '@/public/temperature.svg';
-import TeaIcon from '@/public/tea.svg';
 
 interface TeaLineProps {
   tea: tea;
@@ -34,38 +39,48 @@ const TeaLine = ({
   return (
     <>
       <div className="grid w-full px-2 py-3 my-3 rounded-md grid-cols-teaLineLayout hover:bg-bgPaper">
-        <div className="flex flex-col">
-          <div className="flex items-baseline">
+        <div className="flex flex-col w-full">
+          <div className="flex items-baseline w-full">
             <p className="text-lg text-center text-accent">{name}</p>
             <p className="ml-2 text-base text-center text-textSecondary">
               {brand.name}
             </p>
           </div>
           {!!country || !!flavor ? (
-            <div className="flex items-baseline mt-1">
-              <p className=" text-textPrimary">
-                {!!country ? `${country} | ` : null} {flavor}
+            <div className="flex items-baseline w-full mt-1">
+              <p className="flex items-center justify-start w-full text-textPrimary">
+                {!!country ? (
+                  <span className="flex items-center mr-5">
+                    <GlobeIcon className="w-5 h-5 mr-2 text-textSecondary" />
+                    {country}
+                  </span>
+                ) : null}
+                {!!flavor ? (
+                  <span className="flex items-center mr-5">
+                    <SparklesIcon className="w-5 h-5 mr-2 text-textSecondary" />
+                    {flavor}
+                  </span>
+                ) : null}
               </p>
             </div>
           ) : null}
 
           {drinking_conditions ? (
-            <p className="mt-1 text-sm text-textPrimary">
+            <span className="flex items-center mt-2 mr-5 text-sm text-textPrimary">
+              <TagIcon className="w-5 h-5 mr-2 text-textSecondary" />
               {drinking_conditions}
-            </p>
+            </span>
           ) : null}
         </div>
 
         <div className="flex items-center justify-center w-full h-full">
-          <TeaIcon className="w-5 h-5 mr-1 fill-current text-textSecondary" />
-
+          <ClockIcon className="w-5 h-5 mr-1 text-textSecondary" />
           <p className="text-textPrimary"> {formatTime(brand_time_s)}</p>
         </div>
 
         <div className="flex items-center justify-center w-full h-full">
           <TemperatureIcon className="w-5 h-5 mr-1 fill-current text-textSecondary" />
-
-          <p className="text-textPrimary">{brand_temperature}</p>
+          <p className="text-textPrimary">{brand_temperature}°</p>
         </div>
 
         <div className="flex items-center justify-end w-full h-full">
